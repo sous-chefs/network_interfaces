aae5e7141227e9011029d2bd9e9e5cf2e7d2ab39
Guilhem Lettron <guilhem@lettron.fr>
2012-06-19 18:09:16 +0200

    version 0.0.9, modifications are quite huge for a 0.0.2 but not enough test by all of you for 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache 2.0"
 description      "Installs/Configures network on Ubuntu and Debian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.9"
 
 supports         "ubuntu", ">= 11.10"
 supports         "debian", ">= 7.0.0"
