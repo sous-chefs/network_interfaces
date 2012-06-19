44d8d785c259dc538b2c7367be99e8b20bf75be2
Guilhem Lettron <guilhem@lettron.fr>
2012-06-19 18:03:21 +0200

    we can now use attributes to complete paramters for some interfaces

---
+++ libraries/default.rb
@@ -0,0 +1,13 @@
+class Chef::Recipe::Network_interfaces
+	def self.conf(interface,workingnode=@node)
+		if workingnode.has_key?("network_interfaces")
+			return workingnode[:network_interfaces][interface]
+		else
+			return {}
+		end
+	end
+
+	def self.value(key,interfaces, resource=@new_resource, workingnode=@node)
+		resource.send(key) ? resource.send(key) : self.conf(interfaces,workingnode).has_key?(key) ? self.conf(interfaces,workingnode)[key] : false
+	end
+end
