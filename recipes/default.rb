3cdcb8bd47d79de23adb34182777ac7fb6152196
Guilhem Lettron <guilhem@lettron.fr>
2012-08-20 12:42:15 +0200

    clarify license. fix #3. thanks @meineerde

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,11 +2,22 @@
 # Cookbook Name:: network
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Author:: Guilhem Lettron <guilhem.lettron@youscribe.com>
 #
-# All rights reserved - Do Not Redistribute
+# Copyright 2012, Societe Publica.
 #
-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 require 'fileutils'
 
 def insert_line_if_no_match(filepath, regex, newline)
