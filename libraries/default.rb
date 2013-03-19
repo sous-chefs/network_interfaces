b3ada0d74ff0de7c485ac659b680b26e96ed631c
Guilhem Lettron <guilhem@lettron.fr>
2013-03-19 12:46:56 +0100

    remove tabs

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,15 +1,15 @@
 class Chef::Recipe::Network_interfaces
-	def self.conf(interface,workingnode=@node)
-		if workingnode.has_key?("network_interfaces") and workingnode["network_interfaces"].has_key?("interface")
-			return workingnode[:network_interfaces][interface]
-		else
-			return {}
-		end
-	end
-  
-	def self.value(key,interfaces, resource=@new_resource, workingnode=@node)
-		!resource.send(key).nil? ? resource.send(key) : self.conf(interfaces,workingnode).has_key?(key) ? self.conf(interfaces,workingnode)[key] : nil
-	end
+  def self.conf(interface,workingnode=@node)
+    if workingnode.has_key?("network_interfaces") and workingnode["network_interfaces"].has_key?("interface")
+      return workingnode[:network_interfaces][interface]
+    else
+      return {}
+    end
+  end
+
+  def self.value(key,interfaces, resource=@new_resource, workingnode=@node)
+    !resource.send(key).nil? ? resource.send(key) : self.conf(interfaces,workingnode).has_key?(key) ? self.conf(interfaces,workingnode)[key] : nil
+  end
 end
 
 
