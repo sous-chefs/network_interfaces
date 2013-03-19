66a116b1827b6e5f12a8d659305f19ad7ca2f638
Guilhem Lettron <guilhem@lettron.fr>
2013-03-19 12:48:29 +0100

    remove any useless default value

--- resources/default.rb
+++ resources/default.rb
@@ -6,24 +6,24 @@ end
 actions :save, :remove
 
 attribute :device, :kind_of => String, :name_attribute => true
-attribute :bridge, :kind_of => [ TrueClass, FalseClass, Array ], :default => false
-attribute :bridge_stp, :kind_of => [ TrueClass, FalseClass, NilClass ], :default => nil
-attribute :bond, :kind_of => [ TrueClass, FalseClass, Array ], :default => false
-attribute :bond_mode, :kind_of => String, :default => nil
-attribute :vlan_dev, :kind_of => String, :default => nil
+attribute :bridge, :kind_of => [ TrueClass, FalseClass, Array ]
+attribute :bridge_stp, :kind_of => [ TrueClass, FalseClass ]
+attribute :bond, :kind_of => [ TrueClass, FalseClass, Array ]
+attribute :bond_mode, :kind_of => String
+attribute :vlan_dev, :kind_of => String
 attribute :onboot, :kind_of => [ TrueClass, FalseClass ], :default => true
-attribute :bootproto, :kind_of => String, :default => nil
-attribute :target, :kind_of => String, :default => nil
-attribute :gateway, :kind_of => String, :default => nil
-attribute :metric, :kind_of => Integer, :default => nil
-attribute :mtu, :kind_of => Integer, :default => nil
-attribute :mask, :kind_of => String, :default => nil
-attribute :network, :kind_of => String, :default => nil
-attribute :broadcast, :kind_of => String, :default => nil
-attribute :pre_up, :kind_of => String, :default => nil
-attribute :up, :kind_of => String, :default => nil
-attribute :post_up, :kind_of => String, :default => nil
-attribute :pre_down, :kind_of => String, :default => nil
-attribute :down, :kind_of => String, :default => nil
-attribute :post_down, :kind_of => String, :default => nil
+attribute :bootproto, :kind_of => String
+attribute :target, :kind_of => String
+attribute :gateway, :kind_of => String
+attribute :metric, :kind_of => Integer
+attribute :mtu, :kind_of => Integer
+attribute :mask, :kind_of => String
+attribute :network, :kind_of => String
+attribute :broadcast, :kind_of => String
+attribute :pre_up, :kind_of => String
+attribute :up, :kind_of => String
+attribute :post_up, :kind_of => String
+attribute :pre_down, :kind_of => String
+attribute :down, :kind_of => String
+attribute :post_down, :kind_of => String
 attribute :custom, :kind_of => Hash
