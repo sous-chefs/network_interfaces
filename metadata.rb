988ca4909564e00313f4767f2b3f3d92f7e1c98d
Guilhem Lettron <guilhem@lettron.fr>
2013-04-04 14:56:35 +0200

    release v0.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache 2.0"
 description      "Installs/Configures network on Ubuntu and Debian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 supports         "ubuntu", ">= 11.04"
 supports         "debian", ">= 6.0"
