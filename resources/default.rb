7b75bc9233333251fed8ff7f6322d214ed920d22
Kraig Amador <Kraig.Amador@ticketmaster.com>
2014-04-23 16:07:14 -0700

    added bond-master and bond-primary to config options

--- resources/default.rb
+++ resources/default.rb
@@ -5,30 +5,32 @@ end
 
 actions :save, :remove
 
-attribute :device,     kind_of: String, name_attribute: true
-attribute :bridge,     kind_of: [TrueClass, FalseClass, Array]
-attribute :bridge_stp, kind_of: [TrueClass, FalseClass]
-attribute :bond,       kind_of: [TrueClass, FalseClass, Array]
-attribute :bond_mode,  kind_of: String
-attribute :vlan_dev,   kind_of: String
-attribute :onboot,     kind_of: [TrueClass, FalseClass], default: true
-attribute :bootproto,  kind_of: String
-attribute :target,     kind_of: String
-attribute :gateway,    kind_of: String
-attribute :metric,     kind_of: Integer
-attribute :mtu,        kind_of: Integer
-attribute :mask,       kind_of: String
-attribute :network,    kind_of: String
-attribute :broadcast,  kind_of: String
-attribute :pre_up,     kind_of: String
-attribute :up,         kind_of: String
-attribute :post_up,    kind_of: String
-attribute :pre_down,   kind_of: String
-attribute :down,       kind_of: String
-attribute :post_down,  kind_of: String
-attribute :custom,     kind_of: Hash
-attribute :ovs_type,   kind_of: String
-attribute :ovs_bridge, kind_of: String
-attribute :ovs_ports,  kind_of: String
-attribute :ovs_bonds,  kind_of: String
-attribute :ovs_options,kind_of: String
+attribute :device,       kind_of: String, name_attribute: true
+attribute :bridge,       kind_of: [TrueClass, FalseClass, Array]
+attribute :bridge_stp,   kind_of: [TrueClass, FalseClass]
+attribute :bond,         kind_of: [TrueClass, FalseClass, Array]
+attribute :bond_mode,    kind_of: String
+attribute :bond_master,  kind_of: String
+attribute :bond_primary, kind_of: String
+attribute :vlan_dev,     kind_of: String
+attribute :onboot,       kind_of: [TrueClass, FalseClass], default: true
+attribute :bootproto,    kind_of: String
+attribute :target,       kind_of: String
+attribute :gateway,      kind_of: String
+attribute :metric,       kind_of: Integer
+attribute :mtu,          kind_of: Integer
+attribute :mask,         kind_of: String
+attribute :network,      kind_of: String
+attribute :broadcast,    kind_of: String
+attribute :pre_up,       kind_of: String
+attribute :up,           kind_of: String
+attribute :post_up,      kind_of: String
+attribute :pre_down,     kind_of: String
+attribute :down,         kind_of: String
+attribute :post_down,    kind_of: String
+attribute :custom,       kind_of: Hash
+attribute :ovs_type,     kind_of: String
+attribute :ovs_bridge,   kind_of: String
+attribute :ovs_ports,    kind_of: String
+attribute :ovs_bonds,    kind_of: String
+attribute :ovs_options,  kind_of: String
