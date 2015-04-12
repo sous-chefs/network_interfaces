600d2757fb17cfb79c18f8e09a514b990466e2f1
Denis Chapligin <chollya@satgate.net>
2015-04-12 23:34:14 +0300

    Added IPv6 support

--- providers/default.rb
+++ providers/default.rb
@@ -37,25 +37,50 @@ action :save do
   end
 
   e = []
+  seen_v4=false
+  seen_v6=false
   target.each_with_index do |t, i|
     if t.include? '/'
-      #Netmask specified in a CIDR format
-      netmask = IPAddr.new('255.255.255.255').mask(t.split('/')[1]).to_s
-      t = t.split('/')[0]
+	#Addres includes netmask in a CIDR format
+	netmask = t.split('/')[1]
+        t = t.split('/')[0]
     else
-      netmask = Chef::Recipe::NetworkInterfaces.value(:mask,       new_resource.device, new_resource, node)
+        netmask = Chef::Recipe::NetworkInterfaces.value(:mask,       new_resource.device, new_resource, node)
     end
+
+    if t.include? ':'
+	#IPv6
+	family = 'inet6'
+    else
+	#IPv4
+        family = 'inet'
+	if !netmask.include? '.'
+    	  #Netmask specified in a CIDR format
+    	  netmask = IPAddr.new('255.255.255.255').mask(netmask).to_s
+	end
+    end
+
     iface_data = {
         "type" =>         type,
+        "family" =>       family,
         "device" =>       new_resource.device,
         "address" =>      t,
         "netmask" =>      netmask
     }
+
+    if !seen_v6 and family=='inet6'
+        iface_data["gateway"] = Chef::Recipe::NetworkInterfaces.value(:gateway6,    new_resource.device, new_resource, node)
+        seen_v6=true
+    end
+    if !seen_v4 and family=='inet'
+        iface_data["gateway"] = Chef::Recipe::NetworkInterfaces.value(:gateway,    new_resource.device, new_resource, node)
+        seen_v4=true
+    end
+
     if i ==0 
 	#We need the whole interface description only at the first address entry
 	iface_data["auto"]=Chef::Recipe::NetworkInterfaces.value(:onboot,     new_resource.device, new_resource, node)
         iface_data["network"] =      Chef::Recipe::NetworkInterfaces.value(:network,    new_resource.device, new_resource, node),
-        iface_data["gateway"] =      Chef::Recipe::NetworkInterfaces.value(:gateway,    new_resource.device, new_resource, node),
         iface_data["broadcast"] =    Chef::Recipe::NetworkInterfaces.value(:broadcast,  new_resource.device, new_resource, node),
         iface_data["bridge_ports"] = Chef::Recipe::NetworkInterfaces.value(:bridge,     new_resource.device, new_resource, node),
         iface_data["bridge_stp"] =   Chef::Recipe::NetworkInterfaces.value(:bridge_stp, new_resource.device, new_resource, node),
