e9fbe7c4186e9205891b267e288530d15bf83574
Denis Chapligin <chollya@satgate.net>
2015-04-13 12:42:23 +0300

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem.lettron@youscribe.com'
 license 'Apache 2.0'
 description 'Installs/Configures network on Ubuntu and Debian'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.0.2'
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 6.0.8'
