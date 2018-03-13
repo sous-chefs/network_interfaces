c7341e3889d545cad25075fe5614c67d24180a46
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-03-13 17:43:56 -0400

    Align metadata for readability

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name 'network_interfaces'
-maintainer 'Guilhem Lettron'
+name             'network_interfaces'
+maintainer       'Guilhem Lettron'
 maintainer_email 'guilhem.lettron@youscribe.com'
-license 'Apache-2.0'
-description 'Installs/Configures network on Ubuntu and Debian'
+license          'Apache-2.0'
+description      'Installs/Configures network on Ubuntu and Debian'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
-chef_version '>= 12.1'
+version          '1.0.1'
+chef_version     '>= 12.1'
 
 issues_url 'https://github.com/redguide/network_interfaces/issues'
 source_url 'https://github.com/redguide/network_interfaces'
