d2121f83d86c6b61070a87c12d47f70cc3c22542
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-03-13 17:43:15 -0400

    Set reasonable chef_version metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license 'Apache-2.0'
 description 'Installs/Configures network on Ubuntu and Debian'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
+chef_version '>= 12.1'
 
 supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 8.0'
